2de0d75e0814dccce42c0f6d596c1439a66a24ff
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2022-11-02 19:59:34 +0200

    adds task 02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const cardMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardMarkup);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `
+			<a class="gallery__item"
+				href="${original}">
+    			<img
+      			class="gallery__image"
+      			src="${preview}"
+      			alt="${description}"
+    			/>
+  		</a>`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
