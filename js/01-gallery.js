a229ed5be327699346000b214b5aba28dc93bf2e
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2022-11-02 17:57:20 +0200

    adds task 01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,61 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector('.gallery');
+const cardMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardMarkup);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `
+			<div class="gallery__item">
+  			<a class="gallery__link" 
+				href="${original}">
+    			<img
+      			class="gallery__image"
+      			src="${preview}"
+      			data-source="${original}"
+      			alt="${description}"
+    			/>
+  			</a>
+			</div>`;
+    })
+    .join('');
+}
+
+function onContainerClick(event) {
+  event.preventDefault();
+
+  const isGalleryCardEl = event.target.dataset.source;
+  if (!isGalleryCardEl) {
+    return;
+  }
+
+  console.log(isGalleryCardEl);
+  console.log(event.target.alt);
+
+  const instance = basicLightbox.create(
+    `
+    <img src="${isGalleryCardEl}" alt="${event.target.alt}">
+`,
+    {
+      onShow: instance => {
+        window.addEventListener('keydown', onEscKeyPress);
+      },
+    }
+  );
+
+  instance.show();
+
+  function onEscKeyPress(event) {
+    if (event.code === 'Escape') {
+      window.removeEventListener('keydown', onEscKeyPress);
+      instance.close();
+    }
+  }
+}
+
+galleryContainer.addEventListener('click', onContainerClick);
+
 console.log(galleryItems);
