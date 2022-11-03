53d8a69a974bb5311d811dcb21cff4666f74ab95
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2022-11-03 13:01:04 +0200

    adds li

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,14 +9,16 @@ function createGalleryMarkup(galleryItems) {
   return galleryItems
     .map(({ original, preview, description }) => {
       return `
-			<a class="gallery__item"
-				href="${original}">
-    			<img
-      			class="gallery__image"
-      			src="${preview}"
-      			alt="${description}"
-    			/>
-  		</a>`;
+			<li>
+				<a class="gallery__item"
+					href="${original}">
+	    			<img
+	      			class="gallery__image"
+	      			src="${preview}"
+	      			alt="${description}"
+	    			/>
+	  		</a>
+			</li>`;
     })
     .join('');
 }
